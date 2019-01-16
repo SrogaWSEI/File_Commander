b4a666cc13c88c16a2f2d2a782c6f7bb1fd8b96d
Lukasz Sroga <lukasz.sroga@microsoft.wsei.edu.pl>
2019-01-16 21:12:19 +0100

    Add second ListView

--- File_explorer/MainWindow.xaml.cs
+++ File_explorer/MainWindow.xaml.cs
@@ -25,13 +25,16 @@ namespace File_explorer
     {
         public MainWindow()
         {
+
             InitializeComponent();
             GettingListDrives();
             GettingFiles();
+            // string text = ListView_1.SelectedItems[0].ToString();
+
         }
 
+       
 
-        
         //Funkcje pobierające dane
 
         //Wrzucanie dysków do combo
@@ -41,30 +44,40 @@ namespace File_explorer
             for (int i=0; i<ListDrives.Length; i++)
             {
                 ListDisk_1.Items.Add(ListDrives[i]);
-            }           
+                ListDisk_2.Items.Add(ListDrives[i]);
+            }
+            
+            
+           
         }
        
     //Wrzucanie plików do Listy
     public void GettingFiles()
         {
-
-            DirectoryInfo disk = new DirectoryInfo(@"C:\");
+            DirectoryInfo disk = new DirectoryInfo(@"D:\");
+            DirectoryInfo disk_2 = new DirectoryInfo(@"C:\");
             FileInfo[] Files = disk.GetFiles();
             DirectoryInfo[] Directories = disk.GetDirectories();
-            //Files
-
+            FileInfo[] Files_2 = disk_2.GetFiles();
+            DirectoryInfo[] Directories_2 = disk_2.GetDirectories();
+            //Files           
             foreach (FileInfo file in Files)
             {
                 ListView_1.Items.Add(file.Name);
+            }
+            foreach (FileInfo file in Files_2)
+            {
+                ListView_2.Items.Add(file.Name);
             }
             //Directonaries
             foreach (DirectoryInfo directory in Directories)
             {
                 ListView_1.Items.Add(directory.Name);
-                
             }
-
-
+            foreach (DirectoryInfo directory in Directories_2)
+            {
+                ListView_2.Items.Add(directory.Name);
+            }
         }
 
         //POBIERANIE Z LISTY
@@ -72,7 +85,7 @@ namespace File_explorer
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            
         }
 
         private void ListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
@@ -82,10 +95,36 @@ namespace File_explorer
 
         private void List_1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (ListDisk_1.SelectedIndex >= 0)
+            {
+                string getdisk = ListDisk_1.SelectedItem.ToString();
+                txt_test.Text = getdisk;
+            }
+            else
+            {
+                string getdisk = @"C:\";
+                txt_test.Text = getdisk;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
-    
+        private void Btn_test_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Txt_test_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+
 
         //Funkcje pobierające dane KONIEC
 
